0f8be7974d2100a29fc01454d6c2b5e1f3d7e14b
Jay Cortes <jaycortes@Jays-Air.fritz.box>
2024-04-03 11:35:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You walk the dog");
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You clean the kitchen");
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("You take out the trash");
+    }, 500);
+  });
+}
+
+walkDog()
+  .then((value) => {
+    console.log(value);
+    return cleanKitchen();
+  })
+  .then((value) => {
+    console.log(value);
+    return takeOutTrash();
+  })
+  .then((value) => {
+    console.log(value);
+    console.log("You are done for today!");
+  });
